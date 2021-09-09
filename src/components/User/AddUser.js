94a8c3bce39a9a8c27ca54ef86467e292b040bb9
kianhaim <kristianhaim@gmail.com>
2021-09-09 16:16:56 -0500

    Styles

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,19 +1,21 @@
 import React from "react";
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 const AddUser = props => {
   const addUserHandler = event => {
     event.prevetnDefault();
   };
+
   return (
-    <Card className={classes.input}>
+    <Card addUserCss={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
         <input type='text' id='username'></input>
         <label htmlFor='age'>Age (Year)</label>
         <input type='number' id='age'></input>
-        <button type='submit'>Add User</button>
+        <Button type='submit'>Add User</Button>
       </form>
     </Card>
   );
